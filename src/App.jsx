57da1379f2faf417c0bfdc40a6ac79af96703a9c
Ann-Kiende <annkiende29@gmail.com>
2025-09-08 12:59:01 +0300

    Add React + Vite starter

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', padding: '2rem' }}>
+      <h1>Hello React + Vite!</h1>
+      <p>If you see this, React is working.</p>
+    </div>
+  );
+}
